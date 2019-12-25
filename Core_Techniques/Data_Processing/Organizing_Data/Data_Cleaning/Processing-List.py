c5b66a3415a658963d7470ea7c55342f18433bc8
pz268LASO <zoupeicheng@lasoplanet.com>
2019-12-25 18:10:52 +0800

    l

--- Core_Techniques/Data_Processing/Organizing_Data/Data_Cleaning/Processing-List.py
+++
@@ -1,7 +0,0 @@
-### This document introduces some common operations of lists in Python
-
-## Concatenate list of lists:
-## Suppose x is an example list
-x=[[1],[2],[3]]
-## If we want [1,2,3] we can do:
-new_list=[x[i][j] for i in range(len(x)) for j in range(len(x[i]))]
